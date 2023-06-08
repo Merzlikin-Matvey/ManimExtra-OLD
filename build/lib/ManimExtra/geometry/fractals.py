b4ae1568d45cf43666cc231707358edfa78d3285
Merzlikin Matvey <merzlikinmatvey01@gmail.com>
2023-06-08 15:28:27 +0300

    Sierpinski carpet

--- build/lib/ManimExtra/geometry/fractals.py
+++ build/lib/ManimExtra/geometry/fractals.py
@@ -35,3 +35,18 @@ def Sierpinski_triangle(n):
         v = VGroup(v, v2, v3).scale(0.5).move_to(Triangle().get_center()).shift(UP)
     v.scale(2).move_to(Triangle().get_center())
     return v
+
+def Sierpinski_carpet(n,color=BLUE):
+    v = Square(side_length=1,color=color).set_opacity(1)
+    for _ in range(n):
+        v.shift(DOWN)
+        x1 = v.copy().shift(2*UP)
+        x2 = v.copy().shift(UP).shift(RIGHT)
+        x3 = v.copy().shift(UP).shift(LEFT)
+        x4 = v.copy().shift(RIGHT)
+        x5 = v.copy().shift(LEFT)
+        x6 = v.copy().shift(2*UP).shift(RIGHT)
+        x7 = v.copy().shift(2*UP).shift(LEFT)
+        v = VGroup(v,x1,x2,x3,x4,x5,x6,x7).scale(1/3)
+    return v.scale(3)
+

ef0f8d972ffd1f656f52dd2977a7ab0222025a48
Merzlikin Matvey <merzlikinmatvey01@gmail.com>
2023-06-10 22:42:48 +0300

    tmp

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ManimExtra',
     author='Merzlikin Matvey',
-    version='0.0.1'
+    version='0.0.1',
+    packages=find_packages()
 )
 
